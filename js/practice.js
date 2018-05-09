2f40d69e24882b5712dc1f34263bb647c54c95c4
SebastianPegoraro <sebapego18@gmail.com>
2018-05-09 08:46:52 -0300

    First Commit

---
+++ js/practice.js
@@ -0,0 +1,10 @@
+var mensaje = "gana la de fuera";
+
+function muestraMensaje() {
+  var mensaje = "gana la de dentro";
+  alert(mensaje);
+}
+
+var parrafo = document.getElementsByTagName('p');
+var primerParrafo = parrafo[0];
+alert(primerParrafo);
